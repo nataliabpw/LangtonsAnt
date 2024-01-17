2a682451425ee7431a589e2f072c6114de8ac6da
nataliabpw <01187514@pw.edu.pl>
2024-01-17 10:12:55 +0100

    Utworzono main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wchar.h>
+#include <locale.h>
+
+// LINE_VERTICAL:│
+// LINE_HORIZONTAL:─
+// LINE_DOWN_RIGHT:┌
+// LINE_DOWN_LEFT:┐
+// LINE_UP_RIGHT:└
+// LINE_UP_LEFT:┘
+// SQUARE_WHITE: 
+// SQUARE_BLACK:█
+// ARROW_NORTH_WHITE:△
+// ARROW_NORTH_BLACK:▲
+// ARROW_EAST_WHITE:▷
+// ARROW_EAST_BLACK:▶
+// ARROW_SOUTH_WHITE:▽
+// ARROW_SOUTH_BLACK:▼
+// ARROW_WEST_WHITE:◁
+// ARROW_WEST_BLACK:◀
+
+int main( int argc, char **argv){
+	return 0;
+}
