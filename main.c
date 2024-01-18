014bae692c56579263bcb6d80237145e833f0831
nataliabpw <01187514@pw.edu.pl>
2024-01-18 17:48:46 +0100

    Usunięto niepotrzebne komentarze

--- main.c
+++ main.c
@@ -8,23 +8,6 @@
 #include "wypisywanie.h"
 #include "ruch.h"
 
-// LINE_VERTICAL:│
-// LINE_HORIZONTAL:─
-// LINE_DOWN_RIGHT:┌
-// LINE_DOWN_LEFT:┐
-// LINE_UP_RIGHT:└
-// LINE_UP_LEFT:┘
-// SQUARE_WHITE: 
-// SQUARE_BLACK:█
-// ARROW_NORTH_WHITE:△
-// ARROW_NORTH_BLACK:▲
-// ARROW_EAST_WHITE:▷
-// ARROW_EAST_BLACK:▶
-// ARROW_SOUTH_WHITE:▽
-// ARROW_SOUTH_BLACK:▼
-// ARROW_WEST_WHITE:◁
-// ARROW_WEST_BLACK:◀
-
 int main( int argc, char **argv){
 	int i, j, pom;
 	int opt;
